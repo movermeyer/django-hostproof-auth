fb198ef5f0af0cbcc52fe6779dbd4a966fcd9c00
Jorge Pintado <j.pintado89@gmail.com>
2014-01-06 12:33:38 -0800

    Add bregistration backend

--- hostprooflogin/views.py
+++ hostprooflogin/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from hostprooflogin.models import *
+
+@csrf_exempt
+@require_POST
+def register(request):
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    encrypted_challenge = request.POST.get('encrypted_challenge')
+    decrypted_challenge = request.POST.get('decrypted_challenge')
+    if username and email and encrypted_challenge and decrypted_challenge:
+        if User.objects.filter(username=username).exists():
+            return HttpResponse(status=409, content="Account Already Exists")
+        user = User.objects.create_user(username=username,
+                                        email=email,
+                                        encrypted_challenge=encrypted_challenge,
+                                        decrypted_challenge=decrypted_challenge)
+        return HttpResponse()
+
+    else:
+        return HttpResponseBadRequest('Invalid or missing parameters')

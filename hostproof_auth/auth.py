543f98f3e07b56a94d6d293ab94c97237a1e3874
Jorge Pintado <j.pintado89@gmail.com>
2014-01-08 17:21:39 -0800

    Rename 'decrypted_challenge' variable to 'challenge' in auth.py

--- hostproof_auth/auth.py
+++ hostproof_auth/auth.py
@@ -2,11 +2,11 @@ from django.contrib.auth.models import check_password
 from hostproof_auth.models import User
 
 class ModelBackend(object):
-    def authenticate(self, username=None, decrypted_challenge=None):
-        if username and decrypted_challenge:
+    def authenticate(self, username=None, challenge=None):
+        if username and challenge:
             try:
                 user = User.objects.get(username=username)
-                if check_password(decrypted_challenge, user.decrypted_challenge):
+                if check_password(challenge, user.challenge):
                     return user
             except User.DoesNotExist:
                 pass

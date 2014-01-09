3deea41ae27cbe03385ec71db23f17e576c3cb3a
Jorge Pintado <j.pintado89@gmail.com>
2014-01-08 17:22:53 -0800

    Add GET and POST challenge requests to views.py

--- hostproof_auth/urls.py
+++ hostproof_auth/urls.py
@@ -4,4 +4,5 @@ from hostproof_auth import views
 
 urlpatterns = patterns('hostproof_auth.views',
     url('^register/$', view='register', name='hostproof_auth_register'),
+    url('^challenge/$', view='challenge', name='hostproof_auth_challenge'),
 )
--- hostproof_auth/views.py
+++ hostproof_auth/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, HttpResponseNotAllowed
+from django.contrib.auth import authenticate, login
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
 from hostproof_auth.models import *
+
+import json
+import rsa
 
 @csrf_exempt
 @require_POST
@@ -19,6 +23,38 @@ def register(request):
                                         encrypted_challenge=encrypted_challenge,
                                         challenge=challenge)
         return HttpResponse()
-
     else:
         return HttpResponseBadRequest('Invalid or missing parameters')
+
+@csrf_exempt
+def challenge(request):
+    if request.method == 'GET':
+        username = request.GET.get('username')
+        if username:
+            try:
+                user = User.objects.get(username=username)
+                return HttpResponse(json.dumps({
+                    'encrypted_challenge' : user.encrypted_challenge,
+                }))
+            except User.DoesNotExist:
+                return HttpResponseNotFound('Account Not Found')
+        else:
+            return HttpResponseBadRequest('Invalid or missing parameters')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        challenge = request.POST.get('challenge')
+        if username and challenge:
+            user = authenticate(username=username, challenge=challenge)
+            if user:
+                login(request, user)
+                (pubkey, privkey) = rsa.newkeys(512)
+                request.session['rsa_private'] = privkey.save_pkcs1()
+                return HttpResponse(json.dumps({
+                    'rsa_public' : pubkey.save_pkcs1(),
+                }))
+            else:
+                return HttpResponse(status=403, content='Bad Credentials')
+        else:
+            return HttpResponseBadRequest('Invalid or missing parameters')
+    else:
+        return HttpResponseNotAllowed()
